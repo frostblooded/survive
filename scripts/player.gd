a3e83ad8bf8427c663d30c4b5a39b6d8478f49a6
Nikolay Danailov <frostblooded@yahoo.com>
2024-10-27 13:59:46 +0200

    Add new sprites for player and background made by me

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,25 +3,10 @@ extends CharacterBody2D
 
 @export var speed: float = 400
 
-@export var animated_sprite: AnimatedSprite2D
+@export var sprite: Sprite2D
 
 func _process(_delta: float) -> void:
     var input_direction: Vector2 = Input.get_vector("player_walk_left", "player_walk_right", "player_walk_up", "player_walk_down")
     velocity = input_direction * speed
     move_and_slide()
-    play_animation()
-
-func play_animation() -> void:
-    animated_sprite.flip_h = false
-
-    if velocity.x > 0:
-        animated_sprite.play("walk_right")
-    elif velocity.x < 0:
-        animated_sprite.play("walk_right")
-        animated_sprite.flip_h = true
-    elif velocity.y > 0:
-        animated_sprite.play("walk_down")
-    elif velocity.y < 0:
-        animated_sprite.play("walk_up")
-    else:
-        animated_sprite.play("idle")
+    sprite.flip_h = velocity.x < 0
